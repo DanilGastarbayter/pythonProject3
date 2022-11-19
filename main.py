de778e969973412fdffe70c06e076aa7d12dcafa
DanilGastarbayter <h2oo2h22@gmail.com>
2022-11-19 14:05:17 +0200

    jjj

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ opener = urllib.request.build_opener()
 response = opener.open('https://uk.wikipedia.org/')
 print(response.read())
 import requests
+res_parse_list = []
 response = requests.get('https://coinmarketcap.com/')
 response_text = response.text
 response_parse = response_text.split('<span>')
@@ -10,4 +11,7 @@ for parse_elem_1 in response_parse:
     if parse_elem_1.startswith('$'):
         for parse_elem_2 in parse_elem_1.split('</span>'):
             if parse_elem_2.startswith('$') and parse_elem_2[1].isdigit():
-                print(parse_elem_2)
+                res_parse_list.append(parse_elem_2)
+btc = res_parse_list[0]
+print(btc)
+
